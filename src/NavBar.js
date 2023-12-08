54b5469b768476633ec11a6251e5ccd719408bd1
meghna <meghnasarkar9820@gmail.com>
2023-12-08 17:20:38 +0530

    1st commit of clearedtalent

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,72 +1,70 @@
 // import Button from "react-bootstrap/Button";
+import "./Navbar.css";
 
 function NavBar() {
   return (
     <>
-      <nav
-        className="navbar navbar-expand-lg bg-body-tertiary"
-        style={{ position: "fixed", width: "100%", top: "0" }}
-      >
-        <div className="container-fluid">
-          <a href="https://www.clearedtalent.com">
-            <img
-              src="https://www.clearedtalent.com/static/media/logo.693de24c.png"
-              alt="clearedTalent"
-              width="350"
-              height="90"
-            />
-          </a>
+      <div class="mainnav">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary mnav">
+          <div className="container-fluid">
+            <a href="	https://www.clearedtalent.com/static/media/logo.693de24c.png">
+              <img
+                src="https://www.clearedtalent.com/static/media/logo.693de24c.png"
+                alt="clearedTalent"
+              />
+            </a>
 
-          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-            <form className="flex">
-              <button className="b1">Register & Take Test</button>
-            </form>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end navmini">
+              <form className="flex">
+                <button className="b1">Register & Take Test</button>
+              </form>
 
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Sign-In
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Customer
-                  </a>
-                </li>
+              <div
+                className="collapse navbar-collapse demo"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">
+                      Sign-In
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Customer
+                    </a>
+                  </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Sign-Up
-                  </a>
-                </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Sign-Up
+                    </a>
+                  </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Request Demo
-                  </a>
-                </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Request Demo
+                    </a>
+                  </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Referrals
-                  </a>
-                </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Referrals
+                    </a>
+                  </li>
 
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    {" "}
-                    Contact Us
-                  </a>
-                </li>
-              </ul>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      {" "}
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 }
--- src/TextArea.js
+++ src/TextArea.js
@@ -1,52 +1,64 @@
+import "./Textarea.css";
+
 function TextArea() {
   return (
     <>
-      <div className="text1">
-        <h1 >
-          <center>TERMS OF USE</center>
-        </h1>
-        Last updated: 2021-06-01
-      
-          <ol>
-            <li>
-              <b>Introduction</b>
-            </li>
+    <div class="componentm">
+      <div class="main shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="tnc">
+          <h1>TERMS OF USE</h1>
+        </div>
+        <div class="update">Last updated: 2021-06-01</div>
+        <div class="head">
+          <strong>1.Introduction</strong>
+          <p class="def">
             Welcome to <b>ClearedTalent</b> (“Company”, “we”, “our”, “us”)!
-            <br></br>
+            <br />
             These Terms of Service (“Terms”, “Terms of Service”) govern your use
-            of our website located at{" "}
-            <a href="www.clearedtalent.com">www.clearedtalent.com</a>{" "}
-            (“Service”, “Services”) operated by ClearedTalent. Your agreement
-            with us includes these Terms. You acknowledge that you have read and
-            understood Terms, and agree to be bound of them. If you do not agree
-            with (or cannot comply with) Terms, then you may not use the
-            Service,but please let us know by emailing at{" "}
+            of our website located at <span>
+            <a href="www.clearedtalent.com" target="_blank">
+            www.clearedtalent.com
+            </a></span>
+            (“Service”, “Services”) operated by <b>ClearedTalent</b>. <br />
+            Your agreement with us includes these Terms. You acknowledge that
+            you have read and understood Terms, and agree to be bound of them.
+            If you do not agree with (or cannot comply with) Terms, then you may
+            not use the Service,but please let us know by emailing at{" "}
             <b>support@clearedtalent.com</b> so we can try to find a solution.
             These Terms apply to all visitors, users and others who wish to
             access or use Service.
-            <li>
-              <b> Communications</b>
-            </li>
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>2.Communications </strong>
+          <p class="def">
             By using our Service, you agree to receive notifications, subscribe
             to newsletters, marketing or promotional materials and other
             information we may send. However, you may opt out of receiving any,
             or all, of these communications from us by emailing us at{" "}
-            <a href="support@clearedtalent.com.">support@clearedtalent.com.</a>
-            <li>
-              <b>Intellectual Property </b>
-            </li>
+            <a href="support@clearedtalent.com">support@clearedtalent.com.</a>
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>3.Intellectual Property </strong>
+          <p class="def">
             Service and its content (excluding personal information provided by
             users), features and functionality are and will remain the exclusive
             property of ClearedTalent and its licensors. You own any personal
             information you provide to us, but you also grant us a non-exclusive
-            license to it. ClearedTalent reserves all of its intellectual
-            property rights in the Services. ClearedTalent logos and other
-            ClearedTalent trademarks, service marks, graphics and logos used for
-            our Services are trademarks or registered trademarks of
-            ClearedTalent.
-            <li>
-              <b>Content </b>
-            </li>
+            license to it.
+            <br /> ClearedTalent reserves all of its intellectual property
+            rights in the Services. ClearedTalent logos and other ClearedTalent
+            trademarks, service marks, graphics and logos used for our Services
+            are trademarks or registered trademarks of ClearedTalent.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>4.Content</strong>
+          <p class="def">
             Content found on or through this Service are used with permission.
             Sharing of content (including without limitation, skill test or
             candidate related information) must be done as per established
@@ -55,50 +67,201 @@ function TextArea() {
             distribute, modify, transmit, reuse, download, store, repost, copy,
             or use said Content, whether in whole or in part, for commercial or
             other purposes, without express advance written permission from us.
+            <br />
             You and ClearedTalent agree that we may access, store, process and
             use any information and personal data that you provide in accordance
             with the terms of the{" "}
             <a href="https://www.clearedtalent.com/privacy-policy">
               Privacy Policy
-            </a>
-            .
-            <li>
-              <b>Accounts </b>
-            </li>
+            </a>.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>5.Accounts</strong>
+          <p class="def">
             When you create an account with us, you guarantee that you are above
             the age of 18, and that the information you provide us is accurate
             at all times. Inaccurate information may result in the immediate
-            termination of your account on Service. You are responsible for
-            maintaining the confidentiality of your account and password,
-            including but not limited to the restriction of access to your
-            computer and/or account. You agree to accept responsibility for any
-            and all activities or actions that occur under your account and/or
-            password, whether your password is with our Service or a third-party
-            service. You must notify us immediately upon becoming aware of any
-            breach of security or unauthorized use of your account. You may not
-            use as a username the name of another person or entity or that is
-            not lawfully available for use, a name or trademark that is subject
-            to any rights of another person or entity other than you, without
-            appropriate authorization. You may not use as a username any name
-            that is offensive, vulgar or obscene. If you are a candidate, you
-            understand that the information made available to ClearedTalent may
-            be shared with prospective employers and/or recruiters for contract
-            or full-time job opportunities. Your profile may contain video
-            interviews or video introduction which may also be shared with
-            prospective employers and/or recruiters for contract or full-time
-            job opportunities. You can hide specific information if
-            ClearedTalent allows such functionality. We reserve the right to
-            refuse service, terminate accounts, remove or edit content, or
-            cancel orders in our sole discretion. If you wish to terminate your
-            account, you may simply discontinue using Service. You can also
-            deactivate your account by emailing us at support@clearedtalent.com.
+            termination of your account on Service.
+            <br />
+            You are responsible for maintaining the confidentiality of your
+            account and password, including but not limited to the restriction
+            of access to your computer and/or account.You agree to accept
+            responsibility for any and all activities or actions that occur
+            under your account and/or password, whether your password is with
+            our Service or a third-party service. You must notify us immediately
+            upon becoming aware of any breach of security or unauthorized use of
+            your account.
+            <br /> You may not use as a username the name of another person or
+            entity or that is not lawfully available for use, a name or
+            trademark that is subject to any rights of another person or entity
+            other than you, without appropriate authorization. You may not use
+            as a username any name that is offensive, vulgar or obscene. <br />
+            If you are a candidate, you understand that the information made
+            available to ClearedTalent may be shared with prospective employers
+            and/or recruiters for contract or full-time job opportunities. Your
+            profile may contain video interviews or video introduction which may
+            also be shared with prospective employers and/or recruiters for
+            contract or full-time job opportunities. You can hide specific
+            information if ClearedTalent allows such functionality. <br />
+            We reserve the right to refuse service, terminate accounts, remove
+            or edit content, or cancel orders in our sole discretion. <br />
+            If you wish to terminate your account, you may simply discontinue
+            using Service. You can also deactivate your account by emailing us
+            at support@clearedtalent.com.
+            <br />
             All provisions of Terms which by their nature should survive
             termination shall survive termination, including, without
             limitation, ownership provisions, warranty disclaimers, indemnity
             and limitations of liability.
-          </ol>
+          </p>
         </div>
-    
+        <div class="head">
+          <strong>6.Prohibited Uses</strong>
+          <p class="def">
+            You may use Service only for lawful purposes and in accordance with
+            Terms. You agree not to use Service:
+            <p>
+              {" "}
+              0.1. In any way that violates any applicable national or
+              international law or regulation.
+              <br /> 0.2. For the purpose of exploiting, harming, or attempting
+              to exploit or harm minors in any way by exposing them to
+              inappropriate content or otherwise.
+              <br /> 0.3. To impersonate or attempt to impersonate Company, a
+              Company employee, another user, or any other person or entity.
+              <br /> 0.4. In any way that infringes upon the rights of others,
+              or in any way is illegal, threatening, fraudulent, or harmful, or
+              in connection with any unlawful, illegal, fraudulent, or harmful
+              purpose or activity.
+              <br /> 0.5. Use any robot, spider, or other automatic device,
+              process, or means to access Service for any purpose, including
+              monitoring or copying any of the material on Service.
+              <br /> 0.6. Use any manual process to monitor or copy any of the
+              material on Service or for any other unauthorized purpose without
+              our prior written consent.
+              <br /> 0.7. Use any device, software, or routine that interferes
+              with the proper working of Service.
+              <br /> 0.8. Introduce any viruses, trojan horses, worms, logic
+              bombs, or other material which is malicious or technologically
+              harmful. <br />
+              0.9. Attempt to gain unauthorized access to, interfere with,
+              damage, or disrupt any parts of Service, the server on which
+              Service is stored, or any server, computer, or database connected
+              to Service. <br />
+              0.10. Attack Service via a denial-of-service attack or a
+              distributed denial-of-service attack.
+              <br /> 0.11. Take any action that may damage or falsify Company
+              rating. <br />
+              0.12. Otherwise attempt to interfere with the proper working of
+              Service.
+            </p>
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>7.Feedback</strong>
+          <p class="def">
+            You may provide us either directly at support@clearedtalent.com or
+            via our account managers feedback concerning errors, suggestions for
+            improvements, ideas, problems, complaints, and other matters related
+            to our Service (“Feedback”). You acknowledge and agree that: (i) you
+            shall not retain, acquire or assert any intellectual property right
+            or other right, title or interest in or to the Feedback; (ii)
+            Company may have development ideas similar to the Feedback; (iii)
+            Feedback does not contain confidential information or proprietary
+            information from you or any third party; and (iv) Company is not
+            under any obligation of confidentiality with respect to the
+            Feedback. In the event the transfer of the ownership to the Feedback
+            is not possible due to applicable mandatory laws, you grant Company
+            and its affiliates an exclusive, transferable, irrevocable,
+            free-of-charge, sub-licensable, unlimited and perpetual right to use
+            (including copy, modify, create derivative works, publish,
+            distribute and commercialize) Feedback in any manner and for any
+            purpose.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>8.Disclaimer of Warranty</strong>
+          <p class="def">
+            THESE SERVICES ARE PROVIDED BY COMPANY ON AN “AS IS” AND “AS
+            AVAILABLE” BASIS. COMPANY MAKES NO REPRESENTATIONS OR WARRANTIES OF
+            ANY KIND, EXPRESS OR IMPLIED, AS TO THE OPERATION OF THEIR SERVICES,
+            OR THE INFORMATION, CONTENT OR MATERIALS INCLUDED THEREIN. YOU
+            EXPRESSLY AGREE THAT YOUR USE OF THESE SERVICES, THEIR CONTENT, AND
+            ANY SERVICES OR ITEMS OBTAINED FROM US IS AT YOUR SOLE RISK.
+            <br /> NEITHER COMPANY NOR ANY PERSON ASSOCIATED WITH COMPANY MAKES
+            ANY WARRANTY OR REPRESENTATION WITH RESPECT TO THE COMPLETENESS,
+            SECURITY, RELIABILITY, QUALITY, ACCURACY, OR AVAILABILITY OF THE
+            SERVICES. WITHOUT LIMITING THE FOREGOING, NEITHER COMPANY NOR ANYONE
+            ASSOCIATED WITH COMPANY REPRESENTS OR WARRANTS THAT THE SERVICES,
+            THEIR CONTENT, OR ANY SERVICES OR ITEMS OBTAINED THROUGH THE
+            SERVICES WILL BE ACCURATE, RELIABLE, ERROR-FREE, OR UNINTERRUPTED,
+            THAT DEFECTS WILL BE CORRECTED, THAT THE SERVICES OR THE SERVER THAT
+            MAKES IT AVAILABLE ARE FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS
+            OR THAT THE SERVICES OR ANY SERVICES OR ITEMS OBTAINED THROUGH THE
+            SERVICES WILL OTHERWISE MEET YOUR NEEDS OR EXPECTATIONS.
+            <br /> COMPANY HEREBY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER
+            EXPRESS OR IMPLIED, STATUTORY, OR OTHERWISE, INCLUDING BUT NOT
+            LIMITED TO ANY WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, AND
+            FITNESS FOR PARTICULAR PURPOSE.
+            <br /> THE FOREGOING DOES NOT AFFECT ANY WARRANTIES WHICH CANNOT BE
+            EXCLUDED OR LIMITED UNDER APPLICABLE LAW.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>9.Limitation of Liability</strong>
+          <p class="def">
+            EXCEPT AS PROHIBITED BY LAW, YOU WILL HOLD US AND OUR OFFICERS,
+            DIRECTORS, EMPLOYEES, AND AGENTS HARMLESS FOR ANY INDIRECT,
+            PUNITIVE, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGE, HOWEVER IT
+            ARISES (INCLUDING ATTORNEYS’ FEES AND ALL RELATED COSTS AND EXPENSES
+            OF LITIGATION AND ARBITRATION, OR AT TRIAL OR ON APPEAL, IF ANY,
+            WHETHER OR NOT LITIGATION OR ARBITRATION IS INSTITUTED), WHETHER IN
+            AN ACTION OF CONTRACT, NEGLIGENCE, OR OTHER TORTIOUS ACTION, OR
+            ARISING OUT OF OR IN CONNECTION WITH THIS AGREEMENT, INCLUDING
+            WITHOUT LIMITATION ANY CLAIM FOR PERSONAL INJURY OR PROPERTY DAMAGE,
+            ARISING FROM THIS AGREEMENT AND ANY VIOLATION BY YOU OF ANY FEDERAL,
+            STATE, OR LOCAL LAWS, STATUTES, RULES, OR REGULATIONS, EVEN IF
+            COMPANY HAS BEEN PREVIOUSLY ADVISED OF THE POSSIBILITY OF SUCH
+            DAMAGE. EXCEPT AS PROHIBITED BY LAW, IF THERE IS LIABILITY FOUND ON
+            THE PART OF COMPANY, IT WILL BE LIMITED TO THE AMOUNT PAID FOR THE
+            PRODUCTS AND/OR SERVICES, AND UNDER NO CIRCUMSTANCES WILL THERE BE
+            CONSEQUENTIAL OR PUNITIVE DAMAGES. SOME STATES DO NOT ALLOW THE
+            EXCLUSION OR LIMITATION OF PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+            DAMAGES, SO THE PRIOR LIMITATION OR EXCLUSION MAY NOT APPLY TO YOU.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>10.Governing Law</strong>
+          <p class="def">
+            These Terms shall be construed in accordance with and governed by
+            the laws of State of Georgia(USA).
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>11.Acknowledgement</strong>
+          <p class="def">
+            BY USING SERVICE OR OTHER SERVICES PROVIDED BY US, YOU ACKNOWLEDGE
+            THAT YOU HAVE READ THESE TERMS OF SERVICE AND AGREE TO BE BOUND BY
+            THEM.
+          </p>
+        </div>
+
+        <div class="head">
+          <strong>12.Introduction</strong>
+          <p class="def">
+            Please send your feedback, comments, requests for technical support
+            by email: <b>support@clearedtalent.com</b>.
+          </p>
+        </div>
+      </div>
+      </div>
     </>
   );
 }
